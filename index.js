d9dcca70578b42ef76d6ec4c6e505540896dea81
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-14 20:47:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+class User {
+  constructor(name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+
+  veto() {
+    return `No, I must disagree`
+  }
+  approve() { 
+  return `You can do that`
+}
+
+}
