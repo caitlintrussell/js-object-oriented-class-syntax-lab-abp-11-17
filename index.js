1ef925440d393fa6bdc95bc7c7ae83c806938ede
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-14 20:54:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,8 @@ class BoardMember {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+class CEO extends BoardMember {
+  hireEmployee() {
+    return `Welcome aboard`
+  }
+}
